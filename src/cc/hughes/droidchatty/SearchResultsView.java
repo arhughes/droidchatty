cf4d54227b1a806fb192fd711203218fb86a9e84
Adam Hughes <arhughes@gmail.com>
2011-03-10 13:24:50 -0500

    remove unused import

--- src/cc/hughes/droidchatty/SearchResultsView.java
+++ src/cc/hughes/droidchatty/SearchResultsView.java
@@ -6,7 +6,6 @@ import android.app.ListActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;

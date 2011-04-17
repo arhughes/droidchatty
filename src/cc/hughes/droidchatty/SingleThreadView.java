aff6ac4be0109833ba660f0f729828df4dadbf48
Adam Hughes <arhughes@gmail.com>
2011-04-17 14:10:18 -0400

    fix duplicated fragment on orientation change

--- src/cc/hughes/droidchatty/SingleThreadView.java
+++ src/cc/hughes/droidchatty/SingleThreadView.java
@@ -15,8 +15,7 @@ public class SingleThreadView extends FragmentActivity
         // create the fragment, and show it!
         ThreadViewFragment frag = new ThreadViewFragment();
         frag.setArguments(getIntent().getExtras());
-        getSupportFragmentManager().beginTransaction().add(
-                android.R.id.content, frag).commit();
+        getSupportFragmentManager().beginTransaction().replace(android.R.id.content, frag).commit();
     }
 }
 

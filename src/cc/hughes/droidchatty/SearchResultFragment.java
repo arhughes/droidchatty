8aa9be2fc5d2f46549013bfcb5d13dd4f9da80d6
Adam Hughes <arhughes@gmail.com>
2011-08-06 17:11:20 -0400

    begin moving search results to fragment

---
+++ src/cc/hughes/droidchatty/SearchResultFragment.java
@@ -0,0 +1,117 @@
+package cc.hughes.droidchatty;
+
+import java.util.ArrayList;
+
+import android.app.SearchManager;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.ListFragment;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ListView;
+import android.widget.TextView;
+
+public class SearchResultFragment extends ListFragment
+{
+    ArrayList<SearchResult> _results;
+    SearchResultsAdapter _adapter;
+    
+    String _term;
+    String _author;
+    String _parentAuthor;
+    
+    int _pageNumber = 0;
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState)
+    {
+        super.onActivityCreated(savedInstanceState);
+        
+        Intent intent = getActivity().getIntent();
+        
+        if (Intent.ACTION_SEARCH.equals(intent.getAction()))
+        {
+            _term = intent.getStringExtra(SearchManager.QUERY);
+            _author = "";
+            _parentAuthor = "";
+        }
+        else
+        {
+            _term = intent.getExtras().getString("terms");
+            _author = intent.getExtras().getString("author");
+            _parentAuthor = intent.getExtras().getString("parentAuthor");
+        }
+        
+        _results = new ArrayList<SearchResult>();
+        _adapter = new SearchResultsAdapter(this.getActivity(), _results);
+        setListAdapter(_adapter);
+        
+        ListView lv = getListView();
+        lv.setOnItemClickListener(new OnItemClickListener()
+        {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id)
+            {
+                displayThread(_adapter.getItem(position));
+            }
+        });
+    }
+    
+    private void displayThread(SearchResult result)
+    {
+        Intent i = new Intent(this.getActivity(), SingleThreadView.class);
+        i.putExtra("postId", result.getPostId());
+        startActivity(i);
+    }
+    
+    private class SearchResultsAdapter extends LoadingAdapter<SearchResult>
+    {
+        public SearchResultsAdapter(Context context, ArrayList<SearchResult> items)
+        {
+            super(context, R.layout.search_result_row, R.layout.row_loading, items);
+        }
+
+        @Override
+        protected View createView(int position, View convertView, ViewGroup parent)
+        {
+            ViewHolder holder = (ViewHolder)convertView.getTag();
+            
+            if (holder == null)
+            {
+                holder = new ViewHolder();
+                holder.userName = (TextView)convertView.findViewById(R.id.textUserName);
+                holder.content = (TextView)convertView.findViewById(R.id.textContent);
+                holder.posted = (TextView)convertView.findViewById(R.id.textPostedTime);
+                convertView.setTag(holder);
+            }
+
+            // get the thread to display and populate all the data into the layout
+            SearchResult t = getItem(position);
+            holder.userName.setText(t.getAuthor());
+            holder.content.setText(PostFormatter.formatContent(t.getAuthor(), t.getContent(), null, false));
+            holder.posted.setText(t.getPosted());
+                    
+            return convertView;
+        }
+        
+        @Override
+        protected ArrayList<SearchResult> loadData() throws Exception
+        {
+            ArrayList<SearchResult> results = ShackApi.search(_term, _author, _parentAuthor, _pageNumber + 1);
+            _pageNumber++;
+            
+            return results;
+        }
+        
+        class ViewHolder
+        {
+            TextView userName;
+            TextView content;
+            TextView posted;
+        }
+        
+    }
+}
--- src/cc/hughes/droidchatty/SearchResultsView.java
+++ src/cc/hughes/droidchatty/SearchResultsView.java
@@ -1,119 +1,14 @@
 package cc.hughes.droidchatty;
 
-import java.util.ArrayList;
-
-import android.app.ListActivity;
-import android.app.SearchManager;
-import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemClickListener;
-import android.widget.ListView;
-import android.widget.TextView;
+import android.support.v4.app.FragmentActivity;
 
-public class SearchResultsView extends ListActivity
+public class SearchResultsView extends FragmentActivity
 {
-    ArrayList<SearchResult> _results;
-    SearchResultsAdapter _adapter;
-    
-    String _term;
-    String _author;
-    String _parentAuthor;
-    
-    int _pageNumber = 0;
-
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.search_results);
-        
-        
-        if (Intent.ACTION_SEARCH.equals(getIntent().getAction()))
-        {
-            _term = getIntent().getStringExtra(SearchManager.QUERY);
-            _author = "";
-            _parentAuthor = "";
-        }
-        else
-        {
-            _term = getIntent().getExtras().getString("terms");
-            _author = getIntent().getExtras().getString("author");
-            _parentAuthor = getIntent().getExtras().getString("parentAuthor");
-        }
-        
-        _results = new ArrayList<SearchResult>();
-        _adapter = new SearchResultsAdapter(this, _results);
-        setListAdapter(_adapter);
-        
-        ListView lv = getListView();
-        lv.setOnItemClickListener(new OnItemClickListener()
-        {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id)
-            {
-                displayThread(_adapter.getItem(position));
-            }
-        });
-        
     }
-    
-    private void displayThread(SearchResult result)
-    {
-        Intent i = new Intent(this, SingleThreadView.class);
-        i.putExtra("postId", result.getPostId());
-        startActivity(i);
-    }
-    
-    private class SearchResultsAdapter extends LoadingAdapter<SearchResult>
-    {
-        public SearchResultsAdapter(Context context, ArrayList<SearchResult> items)
-        {
-            super(context, R.layout.search_result_row, R.layout.row_loading, items);
-        }
-
-        @Override
-        protected View createView(int position, View convertView, ViewGroup parent)
-        {
-            ViewHolder holder = (ViewHolder)convertView.getTag();
-            
-            if (holder == null)
-            {
-                holder = new ViewHolder();
-                holder.userName = (TextView)convertView.findViewById(R.id.textUserName);
-                holder.content = (TextView)convertView.findViewById(R.id.textContent);
-                holder.posted = (TextView)convertView.findViewById(R.id.textPostedTime);
-                convertView.setTag(holder);
-            }
-
-            // get the thread to display and populate all the data into the layout
-            SearchResult t = getItem(position);
-            holder.userName.setText(t.getAuthor());
-            holder.content.setText(PostFormatter.formatContent(t.getAuthor(), t.getContent(), null, false));
-            holder.posted.setText(t.getPosted());
-                    
-            return convertView;
-        }
-        
-        @Override
-        protected ArrayList<SearchResult> loadData() throws Exception
-        {
-            ArrayList<SearchResult> results = ShackApi.search(_term, _author, _parentAuthor, _pageNumber + 1);
-            _pageNumber++;
-            
-            return results;
-        }
-        
-        class ViewHolder
-        {
-            TextView userName;
-            TextView content;
-            TextView posted;
-        }
-        
-    }
-
 }

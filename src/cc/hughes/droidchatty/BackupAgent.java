32c351db2f566437e1ea10e37debbd6383839389
Adam Hughes <arhughes@gmail.com>
2012-07-19 22:22:24 -0400

    Ensure preferences name is correct for backup.

--- src/cc/hughes/droidchatty/BackupAgent.java
+++ src/cc/hughes/droidchatty/BackupAgent.java
@@ -5,13 +5,17 @@ import android.app.backup.SharedPreferencesBackupHelper;
 
 public class BackupAgent extends BackupAgentHelper 
 {
-	static final String PREFS = "cc.hughes.droidchatty_preferences";
 	static final String MY_PREFS_BACKUP_KEY = "preferences";
 	
 	public void onCreate()
 	{
-		SharedPreferencesBackupHelper helper = new SharedPreferencesBackupHelper(this, PREFS);
+		SharedPreferencesBackupHelper helper = new SharedPreferencesBackupHelper(this, getPreferencesName());
 		addHelper(MY_PREFS_BACKUP_KEY, helper);
+	}
+	
+	String getPreferencesName()
+	{
+	    return this.getPackageName() + "_preferences";
 	}
 	
 }

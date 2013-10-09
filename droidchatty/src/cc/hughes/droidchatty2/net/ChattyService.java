a92fa3ab98420b52104fc2a16eae9949ff443b66
Adam Hughes <arhughes@gmail.com>
2013-10-08 23:08:30 -0400

    Support logging in and fetching messages.

--- droidchatty/src/cc/hughes/droidchatty2/net/ChattyService.java
+++ droidchatty/src/cc/hughes/droidchatty2/net/ChattyService.java
@@ -1,7 +1,7 @@
 package cc.hughes.droidchatty2.net;
 
 import android.content.SharedPreferences;
-import android.util.Base64;
+import android.util.Log;
 
 import com.squareup.wire.Wire;
 
@@ -11,31 +11,41 @@ import org.apache.http.message.BasicNameValuePair;
 import org.json.JSONObject;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.StringWriter;
+import java.net.CookieHandler;
+import java.net.CookieManager;
+import java.net.HttpCookie;
 import java.net.HttpURLConnection;
-import java.net.URL;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.List;
 
+import cc.hughes.droidchatty2.text.MessageParser;
+
 public class ChattyService {
+
+    final static String TAG = "ChattyService";
 
     final static String USER_AGENT = "DroidChatty/2.0";
     final static String BASE_URL = "http://shackapidev.hughes.cc/v2/";
-
     final static String SHACKNEWS_POST_URL = "http://www.shacknews.com/api/chat/create/17.json";
+    final static String SHACKNEWS_SIGN_IN = "https://www.shacknews.com/account/signin";
+    final static String SHACKNEWS_MESSAGE_GET = "https://www.shacknews.com/messages/";
 
     final static String FAKE_STORY_ID = "17";
     final static String FAKE_NEWS_ID = "2";
 
     SharedPreferences mPreferences;
+    CookieManager mCookieManager;
 
-    public ChattyService() { }
+    public ChattyService() {
+        mCookieManager = new CookieManager();
+        CookieHandler.setDefault(mCookieManager);
+    }
 
     public ChattyService(SharedPreferences preferences) {
+        this();
         mPreferences = preferences;
     }
 
@@ -49,13 +59,11 @@ public class ChattyService {
     }
 
     public ThreadList getPage(int page) throws IOException {
+        Http.RequestSettings settings = new Http.RequestSettings();
+        settings.Url =  BASE_URL + "page.php?page=" + page;
 
-        URL url = new URL(BASE_URL + "page.php?page=" + page);
-
-        HttpURLConnection connection = (HttpURLConnection)url.openConnection();
+        HttpURLConnection connection = Http.open(settings);
         try {
-            connection.setRequestProperty("User-Agent", USER_AGENT);
-
             Wire wire = new Wire();
             return wire.parseFrom(connection.getInputStream(), ThreadList.class);
         } finally {
@@ -65,12 +73,11 @@ public class ChattyService {
     }
     
     public Thread getThread(String id) throws IOException {
+        Http.RequestSettings settings = new Http.RequestSettings();
+        settings.Url =  BASE_URL + "thread.php?id=" + id;
 
-        URL url = new URL(BASE_URL + "thread.php?id=" + id);
-
-        HttpURLConnection connection = (HttpURLConnection)url.openConnection();
+        HttpURLConnection connection = Http.open(settings);
         try {
-            connection.setRequestProperty("User-Agent", USER_AGENT);
             Wire wire = new Wire();
             return wire.parseFrom(connection.getInputStream(), Thread.class);
         } finally {
@@ -80,10 +87,10 @@ public class ChattyService {
     }
 
     public ThreadList search(int page, String terms, String author, String parentAuthor) throws IOException {
+        Http.RequestSettings settings = new Http.RequestSettings();
+        settings.Url = BASE_URL + "search.php?page=" + page + "&terms=" + URLEncoder.encode(terms, "UTF-8");
 
-        URL url = new URL(BASE_URL + "search.php?page=" + page + "&terms=" + URLEncoder.encode(terms, "UTF-8"));
-
-        HttpURLConnection connection = (HttpURLConnection)url.openConnection();
+        HttpURLConnection connection = Http.open(settings);
         try {
             connection.setRequestProperty("User-Agent", USER_AGENT);
 
@@ -98,8 +105,11 @@ public class ChattyService {
     public int post(int parentId, String content, boolean isNewsThread) throws Exception {
         requirePreferences();
 
-        String userName = mPreferences.getString("pref_shacknews_user", null);
-        String password = mPreferences.getString("pref_shacknews_password", null);
+        Http.RequestSettings settings = new Http.RequestSettings();
+
+        settings.Url = SHACKNEWS_POST_URL;
+        settings.Username = mPreferences.getString("pref_shacknews_user", null);
+        settings.Password = mPreferences.getString("pref_shacknews_password", null);
 
         List<NameValuePair> values = new ArrayList<NameValuePair>();
         values.add(new BasicNameValuePair("content_type_id", isNewsThread ? FAKE_NEWS_ID : FAKE_STORY_ID));
@@ -108,35 +118,9 @@ public class ChattyService {
         if (parentId > 0)
             values.add(new BasicNameValuePair("parent_id", Integer.toString(parentId)));
 
-        String encoded = Base64.encodeToString((userName + ":" + password).getBytes(), Base64.NO_WRAP);
-        UrlEncodedFormEntity e = new UrlEncodedFormEntity(values);
-        String output;
+        settings.PostContent = new UrlEncodedFormEntity(values);
 
-        URL url = new URL(SHACKNEWS_POST_URL);
-        HttpURLConnection connection = (HttpURLConnection)url.openConnection();
-        try {
-            connection.setRequestProperty("User-Agent", USER_AGENT);
-            connection.setRequestProperty("Authorization", "Basic " + encoded);
-
-            OutputStream os = connection.getOutputStream();
-            try {
-                e.writeTo(os);
-                os.flush();
-            } finally {
-                os.close();
-            }
-
-            InputStream is = connection.getInputStream();
-            try {
-                output = readStream(is);
-            } finally {
-                is.close();
-            }
-
-        } finally {
-            connection.disconnect();
-        }
-
+        String output = Http.request(settings);
 
         JSONObject result = new JSONObject(output);
         if (!result.has("data"))
@@ -145,9 +129,71 @@ public class ChattyService {
         return result.getJSONObject("data").getInt("post_insert_id");
     }
 
-    static String readStream(InputStream is) {
-        java.util.Scanner s = new java.util.Scanner(is).useDelimiter("\\A");
-        return s.hasNext() ? s.next() : "";
+    public List<Message> getMessages(String folder, int page) throws Exception {
+        requirePreferences();
+
+        // make sure we are trying for the right thing
+        if (!folder.equals(Message.FOLDER_INBOX) && !folder.equals(Message.FOLDER_SENT))
+            throw new Exception("Invalid message folder: " + folder);
+
+        login();
+
+        Http.RequestSettings settings = new Http.RequestSettings();
+        settings.Url = SHACKNEWS_MESSAGE_GET + folder + "?page=" + page;
+
+        String output = Http.request(settings);
+
+        MessageParser parser = new MessageParser(output);
+        return parser.parse();
     }
+
+    private boolean login() throws Exception {
+        requirePreferences();
+
+        // already have a login cookie?
+        HttpCookie loginCookie = getCookie(SHACKNEWS_SIGN_IN, "_shack_li_");
+        if (loginCookie != null && !loginCookie.hasExpired()) {
+            Log.d(TAG, "Reusing existing login cookie.");
+            return true;
+        }
+
+        Log.d(TAG, "No existing login cookie, logging in.");
+
+        String username = mPreferences.getString("pref_shacknews_user", null);
+        String password = mPreferences.getString("pref_shacknews_password", null);
+
+        List<NameValuePair> values = new ArrayList<NameValuePair>();
+        values.add(new BasicNameValuePair("get_fields[]", "result"));
+        values.add(new BasicNameValuePair("user-identifier", username));
+        values.add(new BasicNameValuePair("supplied-pass", password));
+        values.add(new BasicNameValuePair("remember-login", "1"));
+
+        Http.RequestSettings settings = new Http.RequestSettings();
+        settings.Url = SHACKNEWS_SIGN_IN;
+        settings.Headers.put("X-Requested-With", "XMLHttpRequest");
+        settings.PostContent = new UrlEncodedFormEntity(values, "UTF-8");
+
+        String output = Http.request(settings);
+
+        Log.d(TAG, "Response from login request: " + output);
+
+        JSONObject response = new JSONObject(output);
+        if (response.has("result")) {
+            JSONObject result = response.getJSONObject("result");
+            return result.getBoolean("valid");
+        }
+
+        return false;
+    }
+
+    HttpCookie getCookie(String url, String name) throws URISyntaxException {
+        for (HttpCookie cookie : mCookieManager.getCookieStore().get(new URI(url))) {
+            if (cookie.getName().equals(name))
+                return cookie;
+        }
+
+        return null;
+    }
+
 
 }

174e7cdec1db39827eb4c2e2d7a9e62570cfe927
Adam Hughes <arhughes@gmail.com>
2013-10-10 17:25:37 -0400

    Parse message times.

--- droidchatty/src/cc/hughes/droidchatty2/util/TimeUtil.java
+++ droidchatty/src/cc/hughes/droidchatty2/util/TimeUtil.java
@@ -5,19 +5,22 @@ import java.util.Locale;
 
 import android.content.Context;
 import android.text.format.DateUtils;
+import android.util.Log;
 
 public class TimeUtil {
     
     static final SimpleDateFormat mShackDateFormat = new SimpleDateFormat("MMM dd, yyyy h:mma zzz", Locale.US);
-    
-    static long parseDateTime(String dateTime)
+    static final SimpleDateFormat mShackMessageDateFormat = new SimpleDateFormat("MMM dd, yyyy, h:mm a", Locale.US);
+
+    static long parseDateTime(String dateTime, SimpleDateFormat format)
     {
         try
         {
-            return mShackDateFormat.parse(dateTime).getTime();
+            return format.parse(dateTime).getTime();
         }
         catch (Exception ex)
         {
+            Log.i("TimeUtil", "Error parsing time: " + dateTime, ex);
         }
         
         return 0;
@@ -25,9 +28,12 @@ public class TimeUtil {
     
     public static String format(Context context, String dateTime)
     {
-    	long time = parseDateTime(dateTime);
+    	long time = parseDateTime(dateTime, mShackDateFormat);
     	
     	if (time == 0)
+            time = parseDateTime(dateTime, mShackMessageDateFormat);
+
+        if (time == 0)
     		return dateTime;
     	
     	return DateUtils.getRelativeDateTimeString(context, time, DateUtils.MINUTE_IN_MILLIS, DateUtils.WEEK_IN_MILLIS, 0).toString();
